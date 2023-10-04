e3e44cb0addd38a99088904c2e9863cdadd94cb3
mohsin-3ssolutions <mohsin.ali@3ssolutions.net>
2023-10-04 18:49:59 +0500

    Server Config Add

---
+++ server.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [{
+        name: "oppo-dev",
+        script: "serve",
+        env: {
+            PM2_SERVE_PATH: "./build",
+            PM2_SERVE_PORT: 3000,
+            PM2_SERVE_SPA: "true",
+            PM2_SERVE_HOMEPAGE: "/index.html"
+        }
+    }]
+}
