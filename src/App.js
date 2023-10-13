a43d18c6682c12e40351e55eb045f0540588ccb2
Muhammad Reshaeel <reshaeelnisar71@gmail.com>
2023-10-13 17:38:01 +0500

    chart

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ import './assentials/js/jquery.min.js';
 import './assentials/js/slick.js';
 import Story from './components/story.jsx';
 import FindProject from './components/findProject';
+import ChartComponent from './components/ganttchart';
 
 function App() {
   const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
@@ -67,7 +68,8 @@ function App() {
         <Route path="/our-services" element={<Services />} />
         <Route path="/our-story" element={<Story />} />
         <Route path="/find-a-project" element={<FindProject />} />
-        
+        {/* <Route path="/chart" element={<ChartComponent />} /> */}
+
         <Route path="/signin" element={<SignIn isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/signup" element={<SignUp isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/signin" element={<SignIn isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
